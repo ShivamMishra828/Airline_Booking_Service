f23f47bfd0f50e14b6428b441d8499fbb8af27c8
ShivamMishra828 <shivam9878522087@gmail.com>
2024-05-10 11:09:20 +0530

    FEAT: Added Booking Model with migration file

---
+++ src/utils/common/enums.js
@@ -0,0 +1,18 @@
+const BOOKING_STATUS = {
+    BOOKED: "booked",
+    PENDING: "pending",
+    CANCELLED: "cancelled",
+    INITIATED: "initiated",
+};
+
+const SEAT_TYPE = {
+    ECONOMY: "economy",
+    PREMIUM_ECONOMY: "premium-economy",
+    BUSINESS: "business",
+    FIRST_CLASS: "first-class",
+};
+
+module.exports = {
+    SEAT_TYPE,
+    BOOKING_STATUS,
+};
